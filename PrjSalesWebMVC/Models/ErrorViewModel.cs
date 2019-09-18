8927b148528be75fc3221af98f825c32ffdc50d0
allan0458 <allanbaianocarioca@gmail.com>
2019-09-18 19:45:58 -0300

    First Model-Controller-View - Department

---
+++ PrjSalesWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrjSalesWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
