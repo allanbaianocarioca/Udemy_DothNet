ae28fb810989531882cae0ec0293b9c8df2e16c5
allan0458 <allanbaianocarioca@gmail.com>
2019-09-18 21:22:27 -0300

    CRUD Scaffolding

---
+++ PrjSalesWebMVC/Data/PrjSalesWebMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrjSalesWebMVC.Models
+{
+    public class PrjSalesWebMVCContext : DbContext
+    {
+        public PrjSalesWebMVCContext (DbContextOptions<PrjSalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PrjSalesWebMVC.Models.Department> Department { get; set; }
+    }
+}
